2fc3097c668e0d8c638a5417b01817d13cbef3e8
IT24101972 <it24101972@my.sliit.lk>
2025-09-05 01:05:11 +0530

    Lab 06

---
+++ Lab Sheet 06/IT24101972.R
@@ -0,0 +1,13 @@
+setwd("C:\\Users\\SANDARU\\Desktop\\PS LAB 06")
+
+#1.(i) Binomial Distribution
+#      Here, Random variable x has Binomial Distribution with n=50 and p=0.85
+#  (ii)
+pbinom(47,50,0.85,lower.tail = FALSE)
+
+
+#2.(i)Customer calls per hour
+#  (ii)Poisson Distribution
+#   Here,Random variable x has Poisson Distribution with lamda=12
+#  (iii)
+dpois(15,12)
