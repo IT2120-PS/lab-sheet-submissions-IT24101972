4837f714459d6a5cf6db93c4763f83c2e4b70e11
IT24101972 <it24101972@my.sliit.lk>
2025-09-16 12:13:26 +0530

    Lab 07

---
+++ Lab Sheet 07/IT24101972.R
@@ -0,0 +1,15 @@
+setwd("C:\\Users\\SANDARU\\Desktop\\ps lab 7")
+
+
+#01
+punif(25,min = 0,max = 40) - punif(10,min = 0,max = 40)
+
+#02
+pexp(2,rate = 1/3)
+
+#03
+#i
+1-pnorm(130,100,15)
+
+#ii
+qnorm(0.95,100,15)
